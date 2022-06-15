80a36c654d21dbc5c6fd4e073a87dbe4af10f04e
CHÍNH <93542231+chinhvnn@users.noreply.github.com>
2022-06-15 09:13:53 +0700

    Delete IVIETTECH/REACTJS/iviettech-demo directory

--- IVIETTECH/REACTJS/iviettech-demo/src/component/common/Constants.js
+++
@@ -1,2 +0,0 @@
-export const LIMIT_TASK_PER_PAGE = 2
-export const DEFAULT_DATA_LIST = []
--- IVIETTECH/REACTJS/iviettech-demo/src/component/common/Divider.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Divider extends Component {
-  render() {
-    return (
-      <hr style={{width: this.props.width}}></hr>
-    )
-  }
-}
